36a2da597c980dd0fd4b8887875d5b9990433ba4
Justin Chang <justinchang1124@gmail.com>
2020-09-09 08:33:11 -0400

    remove obselete packaging system

--- packaging.R
+++ packaging.R
@@ -9,71 +9,6 @@ source("~/Justin-Tool/shiny-dim-reduction/scaling.R")
 # -------------------
 # PACKAGE PCA and VAE
 # -------------------
-
-# normal
-for (emb in c("PCA", "VAE"))
-{
-  for (sca in sca_options)
-  {
-    for (nor in nor_options)
-    {
-      for (fea in c(1, 10, 100))
-      {
-        # explore and sum
-        setwd(pro_loc)
-        explore <- my_empty_list(name_cat)
-        sum <- explore 
-        for (cat in names(categories))
-        {
-          explore[[cat]] <- my_empty_list(sub_groups[[cat]])
-          for (sub in sub_groups[[cat]])
-          {
-            loc <- sprintf("%s_%s_%s_%s_%s.rds", fea, nor, sca, sub, cat)
-            explore[[cat]][[sub]] <- myRDS(
-              sprintf("vis-%s/NONE_%s_%s", emb, emb, loc))
-            sum[[cat]][[sub]] <- myRDS(
-              sprintf("vis-%s/SUM_%s_%s", emb, emb, loc))
-          }
-        }
-        setwd(dat_loc)
-        myRDS(make_file_name(
-          sca, nor, fea, emb, "Explore", "", ""),
-          explore)
-        myRDS(make_file_name(
-          sca, nor, fea, emb, "Summarize", "", ""),
-          sum)
-        
-        # tsne
-        for (nei in perplexity_types)
-        {
-          for (dim in c(2,3))
-          {
-            setwd(pro_loc)
-            tsne <- my_empty_list(name_cat)
-            for (cat in names(categories))
-            {
-              tsne[[cat]] <- my_empty_list(sub_groups[[cat]])
-              for (sub in sub_groups[[cat]])
-              {
-                loc <- sprintf("%s_%s_%s_%s_%s.rds", fea, nor, sca, sub, cat)
-                tsne_vis <- myRDS(sprintf("vis-%s/TSNE_%s_%s", emb, emb, loc))
-                tsne[[cat]][[sub]] <- tsne_vis[[
-                  sprintf("TSNE%s", dim)]][[sprintf("P%s", nei)]]
-              }
-            }
-            setwd(dat_loc)
-            myRDS(make_file_name(
-              sca, nor, fea, emb, "tSNE", dim, 
-              which(perplexity_types == nei)),
-              tsne)
-          }
-        }
-      }
-    }
-  }
-}
-
-# AWS
 setwd(pro_loc)
 dog <- names(categories)
 for (cat in dog)
@@ -140,81 +75,8 @@ for (cat in dog)
 # ------------
 # PACKAGE UMAP
 # ------------
-
-# normal
-for (sca in sca_options)
-{
-  for (nor in nor_options)
-  {
-    for (fea in c(1, 10, 100))
-    {
-      # sum
-      setwd(pro_loc)
-      sum <- my_empty_list(name_cat)
-      for (cat in names(categories))
-      {
-        sum[[cat]] <- my_empty_list(sub_groups[[cat]])
-        for (sub in sub_groups[[cat]])
-        {
-          loc <- sprintf("%s_%s_%s_%s_%s.rds", fea, nor, sca, sub, cat)
-          sum[[cat]][[sub]] <- myRDS(sprintf("vis-UMAP/SUM_UMAP_%s", loc))
-        }
-      }
-      setwd(dat_loc)
-      myRDS(make_file_name(
-        sca, nor, fea, "UMAP", "Summarize", "", ""),
-        sum)
-      
-      # explore and tsne
-      for (nei in perplexity_types)
-      {
-        setwd(pro_loc)
-        explore <- my_empty_list(name_cat)
-        for (cat in names(categories))
-        {
-          explore[[cat]] <- my_empty_list(sub_groups[[cat]])
-          for (sub in sub_groups[[cat]])
-          {
-            loc <- sprintf("%s_%s_%s_%s_%s.rds", fea, nor, sca, sub, cat)
-            umap_vis <- myRDS(sprintf("vis-UMAP/NONE_UMAP_%s", loc))
-            explore[[cat]][[sub]] <- umap_vis[[sprintf("P%s", nei)]]
-          }
-        }
-        setwd(dat_loc)
-        myRDS(make_file_name(
-          sca, nor, fea, "UMAP", "Explore", "",
-          which(perplexity_types == nei)),
-          explore)
-        
-        for (dim in c(2,3))
-        {
-          setwd(pro_loc)
-          tsne <- my_empty_list(name_cat)
-          for (cat in names(categories))
-          {
-            tsne[[cat]] <- my_empty_list(sub_groups[[cat]])
-            for (sub in sub_groups[[cat]])
-            {
-              loc <- sprintf("%s_%s_%s_%s_%s.rds", fea, nor, sca, sub, cat)
-              tsne_vis <- myRDS(sprintf("vis-UMAP/TSNE_UMAP_%s", loc))
-              tsne[[cat]][[sub]] <- tsne_vis[[
-                sprintf("TSNE%s", dim)]][[sprintf("P%s", nei)]]
-            }
-          }
-          setwd(dat_loc)
-          myRDS(make_file_name(
-            sca, nor, fea, "UMAP", "tSNE", dim,
-            which(perplexity_types == nei)),
-            tsne)
-        }
-      }
-    }
-  }
-}
-
-# AWS
 setwd(pro_loc)
-dog <- rev(names(categories))
+dog <- names(categories)
 for (cat in dog)
 {
   print(cat)
@@ -278,43 +140,8 @@ for (cat in dog)
 # -------------
 # PACKAGE PHATE
 # -------------
-
-# normal
-for (sca in sca_options)
-{
-  for (nor in nor_options)
-  {
-    for (fea in c(1, 10, 100))
-    {
-      for (nei in perplexity_types)
-      {
-        for (dim in c(2,3))
-        {
-          setwd(pro_loc)
-          phate <- my_empty_list(name_cat)
-          for (cat in names(categories))
-          {
-            phate[[cat]] <- my_empty_list(sub_groups[[cat]])
-            for (sub in sub_groups[[cat]])
-            {
-              phate[[cat]][[sub]] <- myRDS(sprintf(
-                "PHATE/PHATE-%s-%s_%s_%s_%s_%s_%s.rds", 
-                nei, dim, fea, nor, sca, sub, cat))$embedding
-            }
-          }
-          setwd(dat_loc)
-          myRDS(make_file_name(sca, nor, fea, 
-                               "PHATE", "", dim, 
-                               which(perplexity_types == nei)), phate)
-        }
-      }
-    }
-  }
-}
-
-# AWS
+dog <- names(categories)
 setwd(pro_loc)
-dog <- rev(names(categories))
 for (cat in dog)
 {
   print(cat)
@@ -358,9 +185,12 @@ for (cat in dog)
   }
 }
 
-# Sets
-num_filters <- 60
+# ------------
+# PACKAGE SETS
+# ------------
 dog <- names(categories)
+num_filters <- 60
+setwd(pro_loc)
 for (cat in dog)
 {
   print(cat)
