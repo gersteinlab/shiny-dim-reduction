eca09b25cb7db60b0f293cfe8c3bad0ce8114110
Justin Chang <justinchang1124@gmail.com>
2020-09-21 10:03:51 -0400

    created a method that allows you to swap projects easily

--- build.R
+++ build.R
@@ -21,36 +21,36 @@ entex <- function(){
 # --------------
 
 root <- "~/Justin-Tool"
-project_name <- "exRNA"
+project_names <- c("exRNA", "ENTEx")
+project_name <- project_names[1]
 
 # roo (root)
 # -- raw (raw)
 # -- pro (processing)
-# -- -- com (combined)
 # -- app (app)
 # -- -- dep (dependencies)
-# -- -- dat (data)
 roo_loc <- sprintf("%s/%s", root, project_name)
 raw_loc <- sprintf("%s/raw", roo_loc)
 pro_loc <- sprintf("%s/processing", roo_loc)
-com_loc <- sprintf("%s/combined", pro_loc)
 app_loc <- sprintf("%s/app", roo_loc)
 dep_loc <- sprintf("%s/dependencies", app_loc)
-dat_loc <- sprintf("%s/data", app_loc)
 
 # ---------
 # FUNCTIONS
 # ---------
 
-# builds the skeleton of the app
-basic_build <- function() {
-  dir.create(roo_loc)
-  dir.create(raw_loc)
-  dir.create(pro_loc)
-  dir.create(com_loc)
-  dir.create(app_loc)
-  dir.create(dep_loc)
-  dir.create(dat_loc)
+switch_project <- function(name)
+{
+  if (missing(name))
+    name <- setdiff(project_names, project_name)[1]
+  
+  project_name <<- project_name
+  
+  roo_loc <<- sprintf("%s/%s", root, project_name)
+  raw_loc <<- sprintf("%s/raw", roo_loc)
+  pro_loc <<- sprintf("%s/processing", roo_loc)
+  app_loc <<- sprintf("%s/app", roo_loc)
+  dep_loc <<- sprintf("%s/dependencies", app_loc)
 }
 
 # update the selected files in the app
