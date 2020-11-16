8ae101f049a6dcccf6c531117c1838007b9bf664
Justin Chang <justinchang1124@gmail.com>
2020-11-16 00:39:54 -0500

    vastly improved installation system

--- installer.R
+++ installer.R
@@ -1,6 +1,11 @@
 # The purpose of this file is to install packages 
 # and run the offline version of the tool.
-# source("~/Justin-Tool/installer.R")
+# source("~/Justin-Tool/shiny-dim-reduction/installer.R")
+
+# prints a single line cleanly
+print_clean <- function(msg){
+  cat(sprintf("%s\n", msg))
+}
 
 # a list of all libraries necessary for the tool
 lib_list <- c(
@@ -37,86 +42,93 @@ extra_lib_list <- c(
 
 # installation function
 install <- function(){
-  print("You are about to install a Dimensionality Reduction Tool.")
-  print("Developed at Gerstein Lab from 2019-2020.")
-  print("The following R packages are necessary:")
-  print(paste(lib_list, collapse = ", "))
-  print("If a package is not already installed, it will be installed.")
-  confirm <- readline(prompt = "
-Please type 'Y' and press enter to continue. 
-Type anything else and press enter to quit.")
+  package_list <- installed.packages()[,1]
+  missing_packages <- lib_list[!(lib_list %in% package_list)]
   
-  if (confirm == "Y")
+  if (length(missing_packages) > 0)
   {
-    for (lib in lib_list)
+    print_clean("The following R packages are missing:")
+    print_clean(paste(lib_list, collapse = ", "))
+    confirm <- readline(prompt = "
+Type 'Y' and press enter to install these packages. 
+Type anything else and press enter to quit.")
+    
+    if (confirm == "Y")
     {
-      if (!require(lib, character.only=TRUE))
+      for (lib in missing_packages)
       {
-        print(sprintf("Installing %s ...", lib))
         install.packages(lib, type="binary", character.only=TRUE)
-        library(lib, character.only=TRUE)
       }
     }
-    
-    print("Do you wish to use this tool's processing
-workflow with your own datasets?")
-    print("If so, the following additional packages are necessary:")
-    print(paste(extra_lib_list, collapse = ", "))
-    print("Please check Anaconda and the README is satisfied before proceeding.")
+    else
+    {
+      print_clean("Not all necessary packages have been installed.
+                  Quitting installation.")
+      invisible()
+    }
+  }
+  
+  extra_missing_packages <- extra_lib_list[!(extra_lib_list %in% package_list)]
+  
+  if (length(extra_missing_packages) > 0)
+  {
+    print_clean("Do you wish to use this tool's data analysis workflow?")
+    print_clean("If so, the following additional packages are necessary:")
+    print_clean(paste(extra_missing_packages, collapse = ", "))
+    print_clean("Please check that the README is satisfied before proceeding.")
     extras <- readline(prompt="
 To proceed with additional package installation, type 'Y' and press enter to continue. 
 Type anything else and press enter to skip this step.")
     
     if (extras == "Y")
     {
-      for (lib in extra_lib_list)
+      for (lib in extra_missing_packages)
       {
-        if (!require(lib, character.only=TRUE, quietly=TRUE))
+        install.packages(lib, type="binary", character.only=TRUE)
+      }
+    }
+  }
+  
+  run_app <- readline(prompt="
+Type 'Y' and press enter to run the app. 
+Type anything else and press enter to quit.")
+  
+  if (run_app == "Y")
+  {
+    while(TRUE)
+    {
+      if ("app" %in% list.files())
+      {
+        break
+      }
+      else
+      {
+        correct_dir <- readline(sprintf("
+The current directory is %s and it does not contain 'app' as a subfolder.
+Please type 'Q' to quit or type in another directory.", getwd()))
+        
+        if (correct_dir == "Q")
         {
-          print(sprintf("Installing %s ...", lib))
-          install.packages(lib, type="binary", character.only=TRUE)
-          library(lib, character.only=TRUE)
+          print_clean("You have quit the installation.")
+          invisible()
+        }
+        else
+        {
+          tryCatch(
+            setwd(correct_dir),
+            error = function(e){
+              print("Failed to change working directory.")
+            }
+          )
         }
       }
     }
     
-    dirCorrect <- FALSE
-    
-    while(!dirCorrect)
-    {
-      print(sprintf("As a reference, the current directory is: %s", getwd()))
-      print("This directory ought to contain 'app' as a direct subfolder.")
-      mydear <- readline(
-        prompt=sprintf("
-Is this correct? Type 'Y' and press enter to continue.
-Type 'Q' and press enter to quit.
-Otherwise, type in an address and press enter to change directories.",
-                       paste(list.files(), collapse=", ")))
-      if (mydear == "Q")
-      {
-        print("You have quit the installation.")
-        return()
-      }
-      if (mydear != "Y")
-      {
-        tryCatch(
-          setwd(mydear),
-          error = function(e){
-            print("Failed to change working directory.")
-          }
-        )
-      }
-      else
-        dirCorrect <- TRUE
-    }
-    
     shiny::runApp("app")
-  }
-  else
-  {
-    print("You have quit the installation.")
   }
 }
 
 # run installation - further notes are located below
-install()
+print_clean("This tool was developed at the Gerstein Lab from 2019-2020.")
+print_clean("If you are running the offline version for the first time, please
+            call the installation function by typing 'install()'.")
