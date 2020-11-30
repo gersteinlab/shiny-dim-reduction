7d5fcf64b242b67ab61e62e352db9701eb61eedd
Justin Chang <justinchang1124@gmail.com>
2020-11-30 14:58:48 -0500

    huge scratchwork reorganization

--- scratchwork.R
+++ scratchwork.R
@@ -1,5 +1,9 @@
 # run the app first to load libraries
 
+# ----------------
+# MEMORY USAGE FUN
+# ----------------
+
 # lists memory usage (not used in app)
 .ls.objects <- function (pos = 1, pattern, order.by, decreasing=FALSE, head=FALSE, n=5) 
 {
@@ -31,74 +35,9 @@ lsos <- function(..., n=10)
   .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
 }
 
-# example upset
-data <- data.frame(matrix(sample(0:1, 100000, replace=TRUE), ncol = 4))
-colnames(data) <- c("ENC001", "ENC002", "ENC003", "ENC004")
-rownames(data) <- sprintf("Gene %s", 1:nrow(data))
-upset <- upset_custom(data.frame(data), TRUE)
-
-# I have no idea what the purpose of this is
-data <- data.frame(matrix(sample(0:1, 16384, replace=TRUE), ncol = 4))
-colnames(data) <- c("ENC001", "ENC002", "ENC003", "ENC004")
-# View(data)
-lol <- data[!duplicated(data), ]
-lol <- lol[rowSums(lol) > 0,]
-lol[,5] <- 0
-colnames(lol)[5] <- "Occurrences"
-
-for (i in 1:nrow(lol))
-{
-  subset <- which(lol[i,] == TRUE)
-  
-  for (j in 1:nrow(data))
-  {
-    if (sum(which(data[j,] == TRUE) %in% subset) == length(which(data[j,] == TRUE))
-        && length(which(data[j,] == TRUE)) > 0)
-      lol[i, 5] <- lol[i, 5]+1
-  }
-}
-# View(lol)
-target <- matrix(nrow=sum(lol[,5]), ncol=4)
-
-index <- 1
-
-for (i in 1:nrow(lol))
-{
-  for (j in 1:lol[i, 5])
-  {
-    for (k in 1:4)
-    {
-      target[index, k] <- as.numeric(lol[i, k])
-    }
-    index <- index+1
-  }
-}
-# View(target)
-
-upset_custom(data)
-rownames(target) <- 1:nrow(target)
-colnames(target) <- c("ENC001", "ENC002", "ENC003", "ENC004")
-upset_custom(data.frame(target))
-
-# df_keep_cols <- function(m)
-# {
-#   cols <- colnames(m)
-#   m <- data.frame(m)
-#   colnames(m) <- cols
-#   m
-# }
-# 
-# df_apply <- function(df, margin, fun)
-# {
-#   apply(df, margin, fun) %>% df_keep_cols
-# }
-# 
-# char_fill <- function(x)
-# {
-#   if (is.nan(x) || is.na(x))
-#     return("")
-#   return(x)
-# }
+# -------------------
+# OLD EXRNA CONVERTER
+# -------------------
 
 # Used to fill missing entries with 0s
 add_missing <- function(table, ref, start, end)
@@ -228,44 +167,111 @@ for (i in 1:len)
 all_gene_data <- all_gene_data[1:sum(all_gene_added)]
 spe_gene_data <- spe_gene_data[1:sum(spe_gene_added)]
 
-# print(dim(pca$rotation))
-# 
-# rotate <- pca$rotation[,1:3]
-# ind <- rep(0, nrow(rotate))
-# for (i in 1:nrow(rotate))
-# {
-#   target <- abs(rotate[i, ])
-#   sum_ind <- which(target > sum(target)*0.5)
-#   
-#   if (length(sum_ind) > 0)
-#     ind[i] <- sum_ind
-# }
-# pca <- prcomp(scaled, center = TRUE, rank. = pc_cap) 
-# lol <- prcomp(pca$rotation, center = TRUE, rank. = 3)
-# target <- lol$x
-# 
-# c1 <- apply(pca$rotation[,1:3], 1, function(x){
-#   target <- abs(x)
-#   sum_ind <- which(target > sum(target)*0.6)
-#   names(sum_ind) <- NULL
-#   
-#   if (length(sum_ind) > 0)
-#     return(sum_ind)
-#   return(0)
-# })
-# indices <- which(c1 != 0)
-# 
-# c2 <- as.factor(c1[indices])
-# bruh <- lol[indices,]
+# -------------
+# EXAMPLE UPSET
+# -------------
 
-factors <- apply(metadata_miRNA, 2, function(x){length(unique(x))})
-print(sprintf("%s: %s", names(factors), factors))
+# example upset
+data <- data.frame(matrix(sample(0:1, 100000, replace=TRUE), ncol = 4))
+colnames(data) <- c("ENC001", "ENC002", "ENC003", "ENC004")
+rownames(data) <- sprintf("Gene %s", 1:nrow(data))
+upset <- upset_custom(data.frame(data), TRUE)
 
-d2 <- strsplit(derp, c("\t", " ", "[", "]"), fixed=TRUE)[[1]]
-d3 <- d2[str_detect(d2, '\\[')]
-d4 <- regStr(d3, " \\[.*", "")
-d5 <- regStr(d4, "\\n.*", "")
+lol <- data[!duplicated(data), ]
+lol <- lol[rowSums(lol) > 0,]
+lol[,5] <- 0
+colnames(lol)[5] <- "Occurrences"
 
+for (i in 1:nrow(lol))
+{
+  subset <- which(lol[i,] == TRUE)
+  
+  for (j in 1:nrow(data))
+  {
+    if (sum(which(data[j,] == TRUE) %in% subset) == length(which(data[j,] == TRUE))
+        && length(which(data[j,] == TRUE)) > 0)
+      lol[i, 5] <- lol[i, 5]+1
+  }
+}
+# View(lol)
+target <- matrix(nrow=sum(lol[,5]), ncol=4)
+
+index <- 1
+
+for (i in 1:nrow(lol))
+{
+  for (j in 1:lol[i, 5])
+  {
+    for (k in 1:4)
+    {
+      target[index, k] <- as.numeric(lol[i, k])
+    }
+    index <- index+1
+  }
+}
+# View(target)
+
+upset_custom(data)
+rownames(target) <- 1:nrow(target)
+colnames(target) <- c("ENC001", "ENC002", "ENC003", "ENC004")
+upset_custom(data.frame(target))
+
+# --------------
+# MISC FUNCTIONS
+# --------------
+
+df_keep_cols <- function(m)
+{
+  cols <- colnames(m)
+  m <- data.frame(m)
+  colnames(m) <- cols
+  m
+}
+
+df_apply <- function(df, margin, fun)
+{
+  apply(df, margin, fun) %>% df_keep_cols
+}
+
+char_fill <- function(x)
+{
+  if (is.nan(x) || is.na(x))
+    return("")
+  return(x)
+}
+
+# ---------------------
+# PCA ROTATION PCA TEST
+# ---------------------
+print(dim(pca$rotation))
+
+rotate <- pca$rotation[,1:3]
+ind <- rep(0, nrow(rotate))
+for (i in 1:nrow(rotate))
+{
+  target <- abs(rotate[i, ])
+  sum_ind <- which(target > sum(target)*0.5)
+
+  if (length(sum_ind) > 0)
+    ind[i] <- sum_ind
+}
+pca <- prcomp(scaled, center = TRUE, rank. = pc_cap)
+lol <- prcomp(pca$rotation, center = TRUE, rank. = 3)
+target <- lol$x
+
+c1 <- apply(pca$rotation[,1:3], 1, function(x){
+  target <- abs(x)
+  sum_ind <- which(target > sum(target)*0.6)
+  names(sum_ind) <- NULL
+
+  if (length(sum_ind) > 0)
+    return(sum_ind)
+  return(0)
+})
+indices <- which(c1 != 0)
+
+c2 <- as.factor(c1[indices])
+bruh <- lol[indices,]
 
 # ----------------------
 # Kun's Methylation Data
@@ -301,24 +307,9 @@ for (dec in names(all_decorations))
   )
 }
 
-# upload Sets
-setwd(dat_loc)
-setwd("Sets")
-filenames <- list.files()
-
-for (file in filenames)
-{
-  data <- myRDS(file)
-  save_db(data, sprintf("Sets/%s", file))
-}
-
-
-
-
-
-# -----
-# exRNA
-# -----
+# ------------------------
+# EXRNA NORMALIZATION TEST
+# ------------------------
 order <- data.frame(matrix(colnames(all_decorations[[1]]), ncol=1))
 colnames(order) <- "TISSUE"
 
@@ -326,7 +317,6 @@ new_order <- my_empty_list(names(all_decorations))
 for (dec in names(all_decorations))
   new_order[[dec]] <- order
 myRDS("order_total.rds", new_order)
-
 
 cat <- name_cat[1]
 combined <- myRDS(sprintf("combined/combined_%s.rds", cat))
@@ -365,7 +355,8 @@ biofluids <- order_total$miRNA$BIOFLUID
 
 tsne <- net_data_tsne[["Logarithmic"]][["Global Min-Max"]]$Y
 plotly_2d(tsne[,1], tsne[,2], "1", "2", "markers", 
-          biofluids, biofluids, color_seq(length(unique(biofluids)), "Rainbow", TRUE), "", TRUE)
+          biofluids, biofluids, 
+          color_seq(length(unique(biofluids)), "Rainbow", TRUE), "", TRUE)
 
 net_data_tsne <- net_data_pca
 for (sca in sca_options)
@@ -378,16 +369,9 @@ for (sca in sca_options)
   }
 }
 
-source("~/Justin-Tool/shiny-dim-reduction/functions.R")
-
-# -----
-# ENTEx
-# -----
-# ---------
-# TRANSPOSE
-# ---------
-source("~/Justin-Tool/shiny-dim-reduction/functions.R")
-
+# ---------------
+# ENTEX TRANSPOSE
+# ---------------
 decor <- decorations$cCREs$Subsets
 ref <- decor$Reference
 decor$Reference <- NULL
@@ -484,11 +468,11 @@ for (cat in dog)
 }
 
 setwd(roo_loc)
-saveRDS(tsne_total, "tsne_total.rds")
-saveRDS(umap_total, "umap_total.rds")
-saveRDS(t1, "t1.rds")
-saveRDS(t2, "t2.rds")
-saveRDS(t3, "t3.rds")
+saveRDS(tsne_total, "Tranpose/tsne_total.rds")
+saveRDS(umap_total, "Tranpose/umap_total.rds")
+saveRDS(t1, "Tranpose/t1.rds")
+saveRDS(t2, "Tranpose/t2.rds")
+saveRDS(t3, "Tranpose/t3.rds")
 
 my_subsetter <- function(types)
 {
@@ -542,7 +526,9 @@ for (cat in dog)
                                t33[s3], t33[s3],  col3, "", TRUE)
 }
 
-# PORTABLE - currently not revised
+# ------------------
+# UNREVISED PORTABLE
+# ------------------
 
 # To begin, install R-Portable and GoogleChromePortable into your dist folder  
 # Add to R-Portable/App/R-Portable/etc/Rprofile.site:  
@@ -662,10 +648,9 @@ Type anything else and press enter to specify a new root.")
 
 set_root()
 
-# Sets
-# --------------
-# USER VARIABLES
-# --------------
+# ------------------
+# FAILED SETS METHOD
+# ------------------
 
 # the maximum number of rows per chunk
 max_feat_chunk <- 20000
@@ -714,8 +699,7 @@ if (length(my_sub) > max_points)
   mar_ind <- which(mar_options == input$mark)
   addr <- sprintf("Sets/%s_%s_%s_%s.rds", cat, sub, sca_ind, mar_ind)
   shinyjs::show("mark")
-}
-else
+} else
 {
   shinyjs::hide("mark")
 }
@@ -798,3 +782,79 @@ do_mark <- function(mar, scaled, num)
 
 # options for set selection (marking features)
 mar_options <- c("Random", "Variance", "Mean", "Maximum")
+
+# --------------
+# SET THRESHOLDS
+# --------------
+
+prec <- 200
+hmm <- data.frame(matrix(0, nrow=prec, ncol=10))
+colnames(hmm) <- dog[1:10]
+
+for (cat in dog[1:10])
+{
+  print(cat)
+  combined <- readRDS(sprintf("combined/combined_%s.rds", cat))
+  scaled <- combined %>% do_scal("Linear", .) %>% do_norm(nor_options[1], .)
+  for (i in 1:prec)
+  {
+    hmm[[cat]][i] <- sum(colSums(scaled >= i/prec) > 0)
+  }
+}
+
+hmm$threshold <- 1:prec/prec
+h2 <- melt(hmm, id.vars = "threshold")
+colors <- color_seq(10, "Rainbow", TRUE)
+plotly_2d(h2$threshold, h2$value,"","","lines+markers", 
+          h2$variable, h2$variable, colors, "", TRUE)
+
+h3 <- hmm
+for (i in 1:10)
+  h3[,i] <- log2(h3[,i])
+h4 <- melt(h3, id.vars="threshold")
+plotly_2d(h4$threshold, h4$value,"","","lines+markers", 
+          h4$variable, h4$variable, colors, "", TRUE)
+
+for (i in 1:9)
+  print_clean(sprintf("%s: %s", dog[i], summary(lm(threshold~get(dog[i]),h3))$r.squared))
+
+for (i in 10)
+  print_clean(sprintf("%s: %s", dog[i], summary(lm(threshold~get(dog[i]),hmm))$r.squared))
+
+# intended for converter
+
+# a version of dplyr::bind_rows that works extremely quickly
+# when binding many small matrices together
+chunk_bind_rows <- function(data, num_chunks=1, bind_fun = dplyr::bind_rows)
+{
+  len <- length(data)
+  
+  if (len < 2)
+    return(data)
+  
+  if (is.null(names(data)))
+    names(data) <- 1:len
+  
+  if (num_chunks < 2)
+    return(bind_fun(data))
+  
+  chunk_size <- ceiling(len/num_chunks)
+  
+  result <- my_empty_list(1:num_chunks)
+  
+  for (i in 1:num_chunks)
+  {
+    min_ind <- (i-1)*chunk_size+1
+    max_ind <- min(len, i*chunk_size)
+    result[[i]] <- data.frame(bind_fun(data[min_ind:max_ind]))
+  }
+  
+  result
+}
+
+# gets the fraction of values of x that are not in 'unacceptable'
+frac_acceptable <- function(x, unacceptable=list(NA, NaN, NULL, "", "Unknown")){
+  1 - sum(x %in% unacceptable)/length(x)
+}
+
+# old entex converter functions
