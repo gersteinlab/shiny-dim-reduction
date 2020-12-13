118f4a414930546689817f9c64d36c99ea569fe0
Justin Chang <justinchang1124@gmail.com>
2020-12-12 21:37:11 -0500

    a new function aiming to shorten certain super-long conditionals

--- ui_functions.R
+++ ui_functions.R
@@ -85,6 +85,15 @@ expand_cond_panel <- function(condition, ...)
   do.call(conditionalPanel, c(condition = condition, ...))
 }
 
+# parses several terms in a string intended to be JS
+repJs <- function(string)
+{
+  repStr(
+    string, c("[EMB]", "[VIS]", "[SUM]", "[PAN]", "[PAN1]", "[PAN2]", "[PAN3]", "[PAN4]"), 
+    c("input.embedding", "input.visualize", "Summarize", "input.plotPanels", pan_options)
+  )
+}
+
 # Creates a selectizeInput panel with only one option allowed.
 select_panel <- function(id, name, options, chosen)
 {
