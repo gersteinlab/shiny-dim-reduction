9b4576fcb0b5638894e029b076992bde55770e4e
Justin Chang <justinchang1124@gmail.com>
2020-12-28 20:30:18 -0500

    starting to work on mnist

---
+++ mnist_converter.R
@@ -0,0 +1,84 @@
+# The goal of this script is to convert test numerical data
+# into numeric data and metadata.
+project_name <- "mnist"
+setwd(sprintf("%s/shiny-dim-reduction", Sys.getenv("SHINY_DIM_REDUCTION_ROOT")))
+source("converter.R", encoding="UTF-8")
+
+categories_full <- list(
+  "MNIST"=list(
+    "Training_Data"=784,
+    "Test_Data"=784
+  )
+)
+
+init_cat()
+
+# from https://tensorflow.rstudio.com/guide/tfestimators/examples/mnist/
+sources <- list(
+  train = list(
+    x = "https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz",
+    y = "https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz"
+  ),
+  test = list(
+    x = "https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz",
+    y = "https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz"
+  )
+)
+
+# read an MNIST file (encoded in IDX format)
+read_idx <- function(file) {
+  # create binary connection to file
+  conn <- gzfile(file, open = "rb")
+  on.exit(close(conn), add = TRUE)
+  
+  # read the magic number as sequence of 4 bytes
+  magic <- readBin(conn, what = "raw", n = 4, endian = "big")
+  ndims <- as.integer(magic[[4]])
+  
+  # read the dimensions (32-bit integers)
+  dims <- readBin(conn, what = "integer", n = ndims, endian = "big")
+  
+  # read the rest in as a raw vector
+  data <- readBin(conn, what = "raw", n = prod(dims), endian = "big")
+  
+  # convert to an integer vecto
+  converted <- as.integer(data)
+  
+  # return plain vector for 1-dim array
+  if (length(dims) == 1)
+    return(converted)
+  
+  # wrap 3D data into matrix
+  matrix(converted, nrow = dims[1], ncol = prod(dims[-1]), byrow = TRUE)
+}
+
+mnist <- rapply(sources, classes = "character", how = "list", function(url) {
+  # download + extract the file at the URL
+  target <- file.path(raw_loc, basename(url))
+  if (!file.exists(target))
+    download.file(url, target)
+  
+  # read the IDX file
+  read_idx(target)
+})
+
+setwd(pro_loc)
+
+saveRDS(mnist$train$x, "combined/combined_Training_Data.rds")
+saveRDS(mnist$test$x, "combined/combined_Test_Data.rds")
+
+order_total <- my_empty_list(name_cat)
+
+order_total[[1]] <- data.frame(matrix(mnist$train$y, ncol=1))
+colnames(order_total[[1]]) <- "Number"
+order_total[[2]] <- data.frame(matrix(mnist$test$y, ncol=1))
+colnames(order_total[[2]]) <- "Number"
+
+amazon_keys <- tester_keys
+pc_cap <- 10
+perplexity_types <- c(50, 400, 1000, 3000, 6000)
+app_title <- "Dimensionality Reduction Tool for MNIST"
+
+setwd(dep_loc)
+self_save(c("categories_full", "amazon_keys", "order_total", "pc_cap",
+            "perplexity_types", "app_title"))
