5a7f37d464cb0c23a96067bc8fbf77c2bf1918fb
Justin Chang <justinchang1124@gmail.com>
2020-12-28 20:30:08 -0500

    setup for testing app functions

---
+++ test_functions.R
@@ -0,0 +1,8 @@
+# This file tests app_functions.R and ui_functions.R
+
+# -----
+# SETUP
+# -----
+
+setwd(sprintf("%s/shiny-dim-reduction", Sys.getenv("SHINY_DIM_REDUCTION_ROOT")))
+source("outline.R", encoding="UTF-8")
