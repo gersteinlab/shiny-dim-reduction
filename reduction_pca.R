72c50baad42c3868e2a36ff35e53e9f303face69
Justin Chang <justinchang1124@gmail.com>
2021-02-18 18:57:36 -0500

    changed whitespace

--- reduction_pca.R
+++ reduction_pca.R
@@ -10,19 +10,19 @@ dog <- name_cat
 for (cat in dog)
 {
   combined <- readRDS(sprintf("combined/combined_%s.rds", cat))
-  
+
   for (sub in sub_groups[[cat]])
   {
     scaled <- get_col_sub(combined, cat, sub)
-    
+
     for (sca in sca_options)
     {
       scaled <- do_scal(sca, scaled)
-      
+
       for (nor in nor_options)
       {
         scaled <- do_norm(nor, scaled)
-        
+
         for (fea in c(1, 10, 100))
         {
           pca_title <- sprintf("PCA/PCA_%s_%s_%s_%s_%s.rds", fea, nor, sca, sub, cat)
@@ -30,7 +30,7 @@ for (cat in dog)
           {
             print(pca_title)
             scaled <- feature_start(scaled, 1.0*fea/100)
-            pca <- prcomp(scaled, center = TRUE, rank. = pc_cap) 
+            pca <- prcomp(scaled, center = TRUE, rank. = pc_cap)
             pca$rotation <- NULL
             pca$center <- NULL
             saveRDS(pca, pca_title)
