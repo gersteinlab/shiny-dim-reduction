b3e07a302caf19dcd4191141bab39d8ddf60fea1
Justin Chang <justinchang1124@gmail.com>
2021-10-22 17:36:06 -0400

    add "***" to headers

--- tests/test_find_replace.R
+++ tests/test_find_replace.R
@@ -20,10 +20,11 @@ source_sdr("find_replace.R")
 
 text_test <- function()
 {
-  print_clean("Functions Tested: rep_str, reg_str")
+  print_clean("*** Functions Tested: rep_str, reg_str ***")
   sprintf_clean("Replace 'A' in 'ABCAAAAA' with 'D': %s", rep_str("ABCAAAAA", "A", "D"))
   sprintf_clean("Replace '.' in 'ABCDE' with 'F': %s", reg_str("ABCDE", ".", "F"))
-  print_clean("Functions Tested: add_perc, rem_perc")
+  print_clean()
+  print_clean("*** Functions Tested: add_perc, rem_perc ***")
   sprintf_clean("Add percent to 100: %s", add_perc(100))
   sprintf_clean("Remove percent from 100%%: %s", rem_perc("100%"))
 }
