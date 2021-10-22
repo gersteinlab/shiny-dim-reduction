5e04de1b11b05e281e3ced98690756f20430aded
Justin Chang <justinchang1124@gmail.com>
2021-10-22 17:45:26 -0400

    moved several tests to test_install

--- tests/test_install.R
+++ tests/test_install.R
@@ -15,3 +15,28 @@ if (!exists("ran_install"))
 # -----
 # TESTS
 # -----
+
+basic_test <- function()
+{
+  print_clean("*** Testing Basic Functions ***")
+  print_clean("This should print cleanly!")
+  print_clean()
+  sprintf_clean("The current time is: %s", Sys.time())
+  start <- my_timer()
+  print_clean("Sleeping for 1 second ...")
+  Sys.sleep(1)
+  sprintf_clean("Now, my_timer(start) yields: %s", my_timer(start))
+  print_clean()
+  sprintf_clean("The result of len_n_list(3) is: %s", paste(len_n_list(3), collapse = ", "))
+  sprintf_clean("The names of empty_named_list(c(\"A\", \"B\", \"C\")) are: %s",
+                paste(names(empty_named_list(c("A", "B", "C"))), collapse = ", "))
+  sprintf_clean("The names of empty_named_list(\"A\", \"B\", \"C\") are: %s",
+                paste(names(empty_named_list("A", "B", "C")), collapse = ", "))
+  sprintf_clean("The result of empty_named_list(NULL) is: [%s] [expected: list()]",
+                paste(empty_named_list(NULL), collapse = ", "))
+}
+
+# -------
+# RUN ALL
+# -------
+basic_test()
--- tests/test_utils.R
+++ tests/test_utils.R
@@ -4,37 +4,23 @@
 # SETUP
 # -----
 
-setwd(sprintf("%s/shiny-dim-reduction", Sys.getenv("SHINY_DIM_REDUCTION_ROOT")))
-source("utils.R", encoding="UTF-8")
+if (!exists("ran_install"))
+{
+  if (file.exists("install.R"))
+    source("install.R")
+  else
+    stop("Could not confirm installation. Please source install.R manually.")
+}
+
+source_sdr("utils.R")
 
 # -----
 # TESTS
 # -----
 
-my_timer_test <- function()
-{
-  print_clean("Functions Tested: my_timer")
-  start <- my_timer()
-  print_clean("Sleeping for 2 seconds ...")
-  Sys.sleep(2)
-  print_clean(sprintf("Time elapsed: %s", my_timer(start)))
-}
 
-my_empty_list_test <- function()
-{
-  print_clean("Functions Tested: my_empty_list")
-  target <- vector(mode="list", length=10)
-  names(target) <- sprintf("P%s", 1:10)
-
-  print_clean("Test all.equal with a conventionally generated list:")
-  print(all.equal(target, my_empty_list(names(target))))
-
-  print_clean("Test NULL as a parameter:")
-  print(my_empty_list(NULL))
-}
 
 # -------
 # RUN ALL
 # -------
-my_timer_test()
-my_empty_list_test()
+
