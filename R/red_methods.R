91bebdd69d20399e9e9c7c74baf05a03204bf9a5
Justin Chang <justinchang1124@gmail.com>
2021-10-30 15:13:35 -0400

    for now, separate sets from the other methods

--- R/red_methods.R
+++ R/red_methods.R
@@ -18,7 +18,6 @@ library(phateR)
 # does not depend on Anaconda
 library(umap)
 library(Rtsne)
-library(Matrix)
 
 # ---------------
 # GENERAL METHODS
@@ -290,86 +289,6 @@ table_to_phate <- function(data, dim, perp) {
     verbose = 1,
     n.jobs = 1,
     seed = 0)
-}
-
-# ------------
-# SETS METHODS
-# ------------
-
-# threshold caps
-lower <- 8 # 2^3
-upper <- 262144 # 2^18
-numdigits <- 4
-len_inter <- 10
-
-# Only allow filtering on a characteristic with <= num_filters distinct values.
-num_filters <- 60
-
-# Given a lower bound, converts into a binary matrix
-calculate_sets <- function(data, low_t) {
-  target <- matrix(as.numeric(data >= low_t), nrow=nrow(data), dimnames = dimnames(data))
-  target[, colSums(target) > 0, drop = FALSE]
-}
-
-# searches for a threshold to numdigits precision
-# such that calculate_sets(data, thre) approximates target
-binary_search <- function(data, target, numdigits)
-{
-  precision <- 0.1^numdigits
-  lower <- precision
-  upper <- 1
-  while (upper-lower >= precision)
-  {
-    mid <- (lower + upper)/2
-
-    if (sum(colSums(data >= mid) > 0) < target)
-      upper <- mid
-    else
-      lower <- mid
-  }
-
-  round((lower+upper)/2,numdigits)
-}
-
-# selects only worthwhile characteristics
-select_chars <- function(order){
-  select_if(order, function(x){
-    between(length(unique(x)), 2, num_filters)
-  })
-}
-
-# rearranges a target matrix based on associated metadata
-gather_char <- function(target, associated){
-  rownames_final <- colnames(target)
-  summary <- summary(Matrix(target, sparse = TRUE))
-  summary_i <- summary[, 1]
-  summary_j <- summary[, 2]
-
-  set_types <- unique(associated)
-  num_types <- length(set_types)
-  rowlen_final <- length(rownames_final)
-
-  final <- rep(0, rowlen_final*num_types)
-  lookup <- match(associated, set_types)
-  lookup2 <- (lookup - 1) * rowlen_final
-
-  for (len in 1:length(summary_i))
-  {
-    num_i <- summary_i[len]
-    index <- lookup2[num_i] + summary_j[len]
-    final[index] <- final[index] + 1
-  }
-
-  final <- matrix(final, ncol=num_types)
-
-  numbers <- rep(0, num_types)
-  for (a in lookup)
-    numbers[a] <- numbers[a] + 1
-  for (j in 1:num_types)
-    final[,j] <- final[,j] / numbers[j]
-
-  dimnames(final) <- list(rownames_final, set_types)
-  final
 }
 
 # ------------
