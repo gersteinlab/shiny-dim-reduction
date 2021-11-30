753dfc623c37298275431dab2e740a6b8d3ac94c
Justin Chang <justinchang1124@gmail.com>
2021-11-29 23:09:08 -0500

    adding test for exRNA

--- tests/test_red_methods.R
+++ tests/test_red_methods.R
@@ -14,61 +14,78 @@ if (!exists("ran_install"))
 
 source_sdr("red_methods.R")
 source_sdr("plotting.R")
+project_name <- "exRNA"
+source_sdr("preprocess.R")
 
+# get random data
 test_table <- matrix(0, nrow = 400, ncol = 20)
+test_labels <- rep(1:5, each = 80)
 
 for (i in 1:nrow(test_table))
   for (j in 1:ncol(test_table))
-    test_table[i,j] <- runif(1)
+    test_table[i,j] <- runif(1, min = 0, max = 0.5 + test_labels[i] / 10)
 
+# test random data
 sprintf_clean("Is this a valid table?: %s", valid_table(test_table))
 
 test_pca <- table_to_pca(test_table, 10)
 test_pca_sum <- pca_to_summary(test_pca)
-plotly_2d(test_pca$x[,1], test_pca$x[,2])
+plotly_2d(test_pca$x[,1], test_pca$x[,2], test_labels)
 plotly_pca_sum(test_pca_sum)
 
 test_vae <- table_to_vae(test_table, 2, 10)
 test_vae_sum <- vae_to_summary(test_vae)
-plotly_2d(test_vae$predict[,1], test_vae$predict[,2])
+plotly_2d(test_vae$predict[,1], test_vae$predict[,2], test_labels)
 plotly_vae_sum(test_vae_sum)
 
 test_umap <- table_to_umap(test_table, 2, 10)
-plotly_heatmap_variance(h2-h1, color_seq(5, "Inferno"), smooth = FALSE)
-
-plotly_2d(test_umap$layout[,1], test_umap$layout[,2])
+plotly_2d(test_umap$layout[,1], test_umap$layout[,2], test_labels)
+plotly_heatmap_variance(knn_label_matrix(umap_to_summary(test_umap), test_labels), smooth = FALSE)
 
 test_phate <- table_to_phate(test_table, 2, 10)
-plotly_2d(test_phate$embedding[,1], test_phate$embedding[,2])
+plotly_2d(test_phate$embedding[,1], test_phate$embedding[,2], test_labels)
 
 test_tsne <- table_to_tsne(test_table, 2, 10)
 plotly_2d(test_tsne$Y[,1], test_tsne$Y[,2])
 
-
-
-
-before <- get.knn(test_table, 5)$nn.index
-test_umap <- table_to_umap(test_table, 2, 10)
-after <- test_umap$knn$indexes
-labels <- rep(1:5, each = 80)
-
-plotly_heatmap_variance(heatmat_before, color_seq(5, "Inferno"), smooth = FALSE)
-
-heatmat_after <- matrix(0, nrow = 5, ncol = 5)
-
+# get miRNA data
 setwd(pro_loc)
+combined_miRNA <- readRDS("combined/combined_miRNA.rds")
 
-pre_image <- readRDS("combined/combined_miRNA.rds")
-post_umap <-  table_to_umap(pre_image, 2, 10)
+feature_start <- function(data, fraction)
+{
+  variances <- apply(data, 2, var)
+  data <- data[,order(variances, decreasing=TRUE),drop=FALSE]
+  num_features <- calc_feat(pc_cap, fraction, ncol(data))
+  data[,1:num_features, drop=FALSE]
+}
 
-start <- get.knn(pre_image)$nn.index
-end <- get.knn(post_umap$layout)$nn.index
+scaled_miRNA <- feature_start(combined_miRNA, 0.1)
+
+mirna_table <- norm_min_max(log_scale(combined_miRNA))
 setwd(dep_loc)
 order_total <- readRDS("order_total.rds")
-labels <- order_total$miRNA$BIOFLUID
+mirna_labels <- order_total$miRNA$CONDITION
 
-h1 <- make_heatmat(start, labels)
-h2 <- make_heatmat(end, labels)
-plotly_heatmap_variance(h2-h1, color_seq(5, "Inferno"), smooth = FALSE)
+# test miRNA data
+sprintf_clean("Is this a valid table?: %s", valid_table(mirna_table))
 
-plotly_heatmap_variance(heatmat_after, color_seq(5, "Inferno"), smooth = FALSE)
+mirna_pca <- table_to_pca(mirna_table, 10)
+mirna_pca_sum <- pca_to_summary(mirna_pca)
+plotly_2d(mirna_pca$x[,1], mirna_pca$x[,2], mirna_labels)
+plotly_pca_sum(mirna_pca_sum)
+
+mirna_vae <- table_to_vae(mirna_table, 2, 10)
+mirna_vae_sum <- vae_to_summary(mirna_vae)
+plotly_2d(mirna_vae$predict[,1], mirna_vae$predict[,2], mirna_labels)
+plotly_vae_sum(mirna_vae_sum)
+
+mirna_umap <- table_to_umap(mirna_table, 2, 10)
+plotly_2d(mirna_umap$layout[,1], mirna_umap$layout[,2], mirna_labels)
+plotly_heatmap_variance(knn_label_matrix(umap_to_summary(mirna_umap), mirna_labels), smooth = FALSE)
+
+mirna_phate <- table_to_phate(mirna_table, 2, 10)
+plotly_2d(mirna_phate$embedding[,1], mirna_phate$embedding[,2], mirna_labels)
+
+mirna_tsne <- table_to_tsne(mirna_table, 2, 10)
+plotly_2d(mirna_tsne$Y[,1], mirna_tsne$Y[,2])
