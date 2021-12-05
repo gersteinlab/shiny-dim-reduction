fdd133a860b0f26953b949edfe4986ab45e61429
Justin Chang <justinchang1124@gmail.com>
2021-12-05 18:07:02 -0500

    moved tsne, added several helper functions, set default perplexity to 0

--- R/red_methods.R
+++ R/red_methods.R
@@ -54,10 +54,40 @@ valid_table <- function(cand_table)
    TRUE
 }
 
+# ------------
+# tSNE METHODS
+# ------------
+
+# rTSNE with parameters set for speed on an m x n matrix
+# dims (d): the number of final features; 1, 2, or 3
+# perlexity (p): the perplexity (nearest neighbors) for tSNE
+# max_iter (i): how many iterations the algorithm should run for
+# theta (t): tradeoff between exact O(n^2) and Barnes-Hut approximation O(n log n)
+# eta (e): learning rate
+# momentum (u): velocity of convergence
+# verbose (v): whether input is displayed as tSNE is run
+# time complexity from tests: O(m^2 n i (d + p))
+table_to_tsne <- function(table, dim = 2, perp = 0, max_iter = 500, theta = 0.5,
+                          eta = 200, momentum = 0.5, verbose = FALSE)
+{
+  # the perplexity can't be too big
+  stopifnot(perp <= floor((nrow(table) - 1)/3))
+  set.seed(0)
+
+  Rtsne(table, dims = dim, perplexity = perp, max_iter = max_iter, theta = theta,
+        eta = eta, momentum = momentum, final_momentum = momentum, verbose = verbose,
+        initial_dims = ncol(table), exaggeration_factor = 1, num_threads = 1,
+        stop_lying_iter = 0, mom_switch_iter = 0,
+        check_duplicates = FALSE, pca = FALSE, partial_pca = FALSE,
+        is_distance = FALSE, Y_init = NULL,
+        pca_center = FALSE, pca_scale = FALSE, normalize = FALSE)
+}
+
 # -----------
 # PCA METHODS
 # -----------
 
+# performs principal component analysis with no scaling on the provided table
 table_to_pca <- function(table, dim = 2)
 {
   pca <- stats::prcomp(table, center = TRUE, rank. = dim)
@@ -66,6 +96,13 @@ table_to_pca <- function(table, dim = 2)
   pca
 }
 
+# accessor helper function
+pca_to_explore <- function(pca_result)
+{
+  pca_result$x
+}
+
+# generates a summary of variance captured vs number of components
 pca_to_summary <- function(pca_result)
 {
   eigs <- pca_result$sdev^2
@@ -76,6 +113,12 @@ pca_to_summary <- function(pca_result)
     "Components" = 1:dim,
     "Variance" = props[1:dim]
   )
+}
+
+# accessor helper function
+pca_to_tsne <- function(pca_result, dim = 2, perp = 0)
+{
+  table_to_tsne(pca_to_explore(pca_result), dim, perp)
 }
 
 # -----------
@@ -231,6 +274,12 @@ table_to_vae <- function(table, dim = 2, batch_size = 2, patience = 10, max_epoc
   )
 }
 
+# accessor helper function
+vae_to_explore <- function(vae_result)
+{
+  vae_result$predict
+}
+
 # extracts loss and val_loss from VAE results and makes a summary
 vae_to_summary <- function(vae_result)
 {
@@ -246,11 +295,17 @@ vae_to_summary <- function(vae_result)
   )
 }
 
+# accessor helper function
+vae_to_tsne <- function(vae_result, dim = 2, perp = 0)
+{
+  table_to_tsne(vae_to_explore(vae_result), dim, perp)
+}
+
 # ------------
 # UMAP METHODS
 # ------------
 
-table_to_umap <- function(table, dim = 2, perp = 2)
+table_to_umap <- function(table, dim = 2, perp = 0)
 {
   umap::umap(
     table,
@@ -268,16 +323,29 @@ table_to_umap <- function(table, dim = 2, perp = 2)
   )
 }
 
+# accessor helper function
+umap_to_explore <- function(umap_result)
+{
+  umap_result$layout
+}
+
+# accessor helper function
 umap_to_summary <- function(umap_result)
 {
   umap_result$knn$indexes
+}
+
+# accessor helper function ... note that the same perplexity is used
+umap_to_tsne <- function(umap_result, dim = 2)
+{
+  table_to_tsne(umap_to_explore(umap_result), dim, umap_result$config$n_neighbors)
 }
 
 # -------------
 # PHATE METHODS
 # -------------
 
-table_to_phate <- function(data, dim = 2, perp = 1) {
+table_to_phate <- function(data, dim = 2, perp = 0) {
   phateR::phate(
     data,
     ndim = dim,
@@ -296,35 +364,6 @@ table_to_phate <- function(data, dim = 2, perp = 1) {
     verbose = 1,
     n.jobs = 1,
     seed = 0)
-}
-
-# ------------
-# tSNE METHODS
-# ------------
-
-# rTSNE with parameters set for speed on an m x n matrix
-# dims (d): the number of final features; 1, 2, or 3
-# perlexity (p): the perplexity (nearest neighbors) for tSNE
-# max_iter (i): how many iterations the algorithm should run for
-# theta (t): tradeoff between exact O(n^2) and Barnes-Hut approximation O(n log n)
-# eta (e): learning rate
-# momentum (u): velocity of convergence
-# verbose (v): whether input is displayed as tSNE is run
-# time complexity from tests: O(m^2 n i (d + p))
-table_to_tsne <- function(table, dim = 2, perp = 1, max_iter = 500, theta = 0.5,
-                     eta = 200, momentum = 0.5, verbose = FALSE)
-{
-  # the perplexity can't be too big
-  stopifnot(perp <= floor((nrow(table) - 1)/3))
-  set.seed(0)
-
-  Rtsne(table, dims = dim, perplexity = perp, max_iter = max_iter, theta = theta,
-        eta = eta, momentum = momentum, final_momentum = momentum, verbose = verbose,
-        initial_dims = ncol(table), exaggeration_factor = 1, num_threads = 1,
-        stop_lying_iter = 0, mom_switch_iter = 0,
-        check_duplicates = FALSE, pca = FALSE, partial_pca = FALSE,
-        is_distance = FALSE, Y_init = NULL,
-        pca_center = FALSE, pca_scale = FALSE, normalize = FALSE)
 }
 
 # ------------
