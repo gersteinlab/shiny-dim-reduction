b574efc5dc40863d8fd12f9bc0e2d18cb5d34a2f
Justin Chang <justinchang1124@gmail.com>
2022-01-03 11:40:32 -0500

    fixed attributes and wrote an index-based perform_reduction

--- R/perform_reduction.R
+++ R/perform_reduction.R
@@ -247,165 +247,368 @@ make_requests <- function(
     return(NULL)
 
   # create a data frame of requests
+  times_requested <- rep(Sys.time(), n_cat)
   requests <- data.frame(
     "CATEGORIES" = cat, "ROW_SUBSETS" = row, "COL_SUBSETS" = col, "SCALING" = sca,
     "NORMALIZATION" = nor, "EMBEDDING" = emb, "VISUALIZATION" = vis, "COMPONENT" = com,
-    "DIMENSION" = dim, "PERPLEXITY" = per, "BATCH_SIZE" = bat, "THRESHOLD" = round(thr, 3)
+    "DIMENSION" = dim, "PERPLEXITY" = per, "BATCH_SIZE" = bat, "THRESHOLD" = round(thr, 3),
     "CHARACTERISTIC" = cha, "AUTHOR" = aut,
-    "TIME_REQUESTED" = rep(Sys.time(), n_cat), "TIME_COMPLETED" = rep(NULL, n_cat)
+    "TIME_REQUESTED" = times_requested, "TIME_COMPLETED" = times_requested - 60 * 60 * 24 # minus 1 day
   )
 
-  # check if every single request is valid
-  for (i in seq_along(numeric(nrow(requests))))
-    if (!is_valid_request(requests[i,]))
+  # cleans all requests, quitting if it's not possible to do so
+  for (i in seq_len(nrow(requests)))
+  {
+    clean_req <- clean_request(requests[i,])
+    if (is.null(clean_req))
       return(NULL)
+    requests[i,] <- clean_req
+  }
 
   requests
 }
 
-
-filename_from_request <- function(request)
+# simplifies the generation of Sets requests
+make_set_requests <- function(
+  cat = character(), sca = character(), thr = numeric(), cha = character(), aut = character())
 {
-  cat <- request$CATEGORIES
-  row <- request$ROW_SUBSETS
-  col <- request$COL_SUBSETS
-  sca <- request$SCALING
-  nor <- request$NORMALIZATION
-  emb <- request$EMBEDDING
-  vis <- request$VISUALIZATION
-  com <- request$COMPONENT
-  dim <- request$DIMENSION
-  per <- request$PERPLEXITY
-  bat <- request$BATCH_SIZE
-  thr <- request$THRESHOLD
-  cha <- request$CHARACTERISTIC
+  n_cat <- length(cat)
 
-  if (emb == "Sets")
-  {
-    return(sprintf("Sets/Sets_%s_%s_%s_%s.rds", cat, sca, thr, cha))
-  }
+  make_requests(
+    cat, chr_d(n_cat), chr_d(n_cat), sca, rep("Global Min-Max", n_cat), rep("Sets", n_cat), chr_d(n_cat),
+    num_d(n_cat), num_d(n_cat), num_d(n_cat), num_d(n_cat), thr, cha, aut)
+}
+
+# simplifies the generation of PHATE requests
+make_phate_requests <- function(
+  cat = character(), row = character(), col = character(), sca = character(),
+  nor = character(), com = numeric(), per = numeric(), aut = character()
+)
+{
+  n_cat <- length(cat)
+
+  make_requests(
+    cat, row, col, sca, nor, rep("PHATE", n_cat), chr_d(n_cat),
+    com, num_d(n_cat), per, num_d(n_cat), num_d(n_cat), chr_d(n_cat), aut)
+}
+
+# -------------------
+# REQUEST FULFILLMENT
+# -------------------
+
+# note: only used for PCA, VAE, UMAP (anything else gets the default value)
+make_inter_name <- function(cat, row, col, sca, nor, emb, com, per, bat)
+{
+  sca_ind <- which(sca_options == sca)
+  nor_ind <- which(nor_options == nor)
 
   if (emb == "PCA")
   {
-
+    return(sprintf("PCA/%s/%s_%s_S%s_N%s_%s.rds",
+                   cat, row, col, sca_ind, nor_ind, com))
   }
+
+  if (emb == "VAE")
+  {
+    return(sprintf("VAE/%s/%s_%s_S%s_N%s_%s_%s.rds",
+                   cat, row, col, sca_ind, nor_ind, com, bat))
+  }
+
+  # UMAP
+  if (emb == "UMAP")
+  {
+    return(sprintf("UMAP/%s/%s_%s_S%s_N%s_%s_%s.rds",
+                   cat, row, col, sca_ind, nor_ind, com, per))
+  }
+
+  chr_d()
 }
 
-# note: valid requests have the following characteristics:
+# converts requests into the locations of their final files
+requests_to_final <- function(requests)
+{
+  result <- character()
+
+  for (i in seq_len(nrow(requests)))
+  {
+    args <- as.list(requests[i,])
+    names(args) <- NULL
+    result <- c(result, do.call(make_sdr_name, args[1:13]))
+  }
+
+  result
+}
+
+# converts a request into the locations of their intermediate files
+requests_to_inter <- function(requests)
+{
+  result <- character()
+
+  for (i in seq_len(nrow(requests)))
+  {
+    args <- as.list(requests[i,])
+    names(args) <- NULL
+    result <- c(result, do.call(make_inter_name, args[c(1:6, 8, 10:11)]))
+  }
+
+  result
+}
+
+
+# finds the index of a request r or returns -1
+# if that request has never been performed before
+index_request <- function(requests, r)
+{
+  for (i in seq_len(nrow(requests)))
+    if (filename_from_request(requests[i]) == requests_to_final(r))
+      return(i)
+  return(-1)
+}
+
+# gives the absolute path of a request with respect to the intermediate file location
+get_inter_loc <- function(request)
+{
+  sprintf("%s/inter/%s", pro_loc, request$INTER_LOC)
+}
+
+# gives the absolute path of a request with respect to the final file location
+get_final_loc <- function(request)
+{
+  sprintf("%s/%s", ref_loc, request$FINAL_LOC)
+}
+
+# get certain rows of requests
+reqs_get <- function(requests, col, val)
+{
+  requests[requests[[col]] == val,]
+}
+
+# readRDS but return NULL if force_inter
+inter_readRDS <- function(force, int_loc)
+{
+  if (force != 2 && file.exists(int_loc))
+    return(readRDS(int_loc))
+  return(NULL)
+}
+
+# syntactic sugar for seq_len(nrow(mat))
+seq_row <- function(mat)
+{
+  seq_len(nrow(mat))
+}
+
+# performs reduction on a group of valid requests,
+# parsing requests in a non-sequential order to maximize speed.
 # force = 0: if a final-level file already exists, do nothing
 # force = 1: override the final-level file
 # force = 2: override all intermediate files and the final-level file
-# note: to do requests in steps, just subset the requests data.frame
-# returns a
+# returns the completed version of requests, with TIME_COMPLETED updated.
+# note: you can control how a subset of reductions are performed like so:
+# subset <- (requests$EMBEDDING == "PCA")
+# requests[subset,] <- perform_reduction(requests[subset,], force)
+# example:
+#     test <- data.frame(matrix(1:25, nrow = 5))
+#     subset <- (test$X1 < 4) & (test$X4 %% 2 == 0)
+#     test[subset,] <- data.frame(matrix(1:10, nrow = 2))
 perform_reduction <- function(requests, force = 0)
 {
+  # used to make intermediate files easily
+  inter_locs <- get_inter_loc(requests_to_inter(requests))
+  # used to make final files easily
+  final_locs <- get_final_loc(requests_to_final(requests))
+  # used to easily edit completion timestamps
+  times_done <- requests$TIME_COMPLETED
+
+  # a true-false vector determining if an analysis should be performed
+  i_fin <- !file.exists(get_final_loc(requests)) | force > 0
+
   # select the category
-  for (cat in unique(requests$CATEGORIES))
+  for (cat in unique(requests$CATEGORIES[i_fin]))
   {
-    # generally, the biggest source of error is if the opened table is not valid
-    cat_table <- readRDS(sprintf("combined/combined_%s.rds", cat))
+    cat_table <- readRDS(sprintf("%s/combined/combined_%s.rds", pro_loc, cat))
+    # if the opened table is not valid, you have a problem
     stopifnot(valid_table(cat_table))
 
-    subrequests_cat <- requests[requests$CATEGORIES == cat,]
+    i_cat <- i_fin & (requests$CATEGORIES == cat)
+    # used for sets
     short_order <- select_chars(order_total[[cat]])
 
     # perform scaling
-    for (sca in unique(subrequests_cat$SCALING))
+    for (sca in unique(requests$SCALING[i_cat]))
     {
       sca_table <- do_scal(sca, cat_table)
-      subrequests_sca <- subrequests_cat[subrequests_cat$SCALING == sca,]
+      i_sca <- i_cat & (requests$SCALING == sca)
 
       # perform normalization
-      for (nor in unique(subrequests_sca$NORMALIZATION))
+      for (nor in unique(requests$NORMALIZATION[i_sca]))
       {
-        subrequests_nor <- subrequests_sca[subrequests_sca$NORMALIZATION == nor,]
         nor_table <- do_norm(nor, sca_table)
+        i_nor <- i_sca & (requests$NORMALIZATION == nor)
+        i_sets <- i_nor & (requests$EMBEDDING == "Sets")
 
-        # sets is unique; solve it first
-        are_set_requests <- (subrequests_nor$EMBEDDING == "Sets")
-        subrequests_sets <- subrequests_nor[are_set_requests,]
-
-        # perform Sets
-        for (thr in unique(subrequests_sets$THRESHOLD))
+        # ----
+        # SETS
+        # ----
+        for (thr in unique(requests$THRESHOLD[i_sets]))
         {
+          i_thr <- i_sets & (requests$THRESHOLD == thr)
           set_result <- table_to_sets(nor_table, thr)
-          subrequests_thr <- subrequests_sets[subrequests_nor$THRESHOLD == thr]
 
-          for (request in subrequests_thr)
+          for (i in which(i_thr)) # only attributes left: characteristic
           {
-            set_loc <- sprintf("Sets/Sets-%s_%s_%s_%s.rds",
-                               ind, sca_ind, cha, cat)
+            r <- requests[i,]
+            f_loc <- get_final_loc(r)
 
-            if (force > 0 || !file.exists(set_loc))
-              saveRDS(set_label_matrix(set_result, short_order), set_loc)
+            sprintf_clean("Generating Sets FINAL: %s", f_loc)
+            set_label_matrix(set_result, short_order[[r$CHARACTERISTIC]]) %>% mkdir_saveRDS(f_loc)
+            times_done[i] <- Sys.time()
           }
         }
 
-        # then move to all other embeddings, which require row / col subsets
-        subrequests_not_sets <- requests[!are_set_requests, ]
-
-        for (row in unique(subrequests_not_sets$ROW_SUBSETS))
+        for (row in unique(requests$ROW_SUBSETS[i_nor]))
         {
-          subrequests_row <- subrequests_not_sets[subrequests_not_sets$ROW_SUBSETS == row,]
           row_table <- get_row_sub(nor_table, cat, row)
+          i_row <- i_nor & (requests$ROW_SUBSETS == row)
 
-          for (col in unique(subrequests_row$COL_SUBSETS))
+          for (col in unique(requests$COL_SUBSETS[i_row]))
           {
-            subrequests_col <- subrequests_row[subrequests_row$COL_SUBSETS == col,]
             col_table <- get_col_sub(row_table, cat, col)
+            i_col <- i_row & (requests$COL_SUBSETS == col)
 
-            for (emb in unique(subrequests_nor$EMBEDDING))
+            for (com in unique(requests$COMPONENT[i_col]))
             {
-              subrequests_emb <- subrequests_col[subrequests_col$EMBEDDING == emb,]
+              i_com <- i_col & (requests$COMPONENT == com)
+              i_pca <- i_com & (requests$EMBEDDING == "PCA")
+              i_vae <- i_com & (requests$EMBEDDING == "VAE")
+              i_umap <- i_com & (requests$EMBEDDING == "UMAP")
+              i_phate <- i_com & (requests$EMBEDDING == "PHATE")
 
-              # print(paste(cat, row, col, sca, nor, emb, sep = ", "))
-
-              if (emb == "PCA")
+              # ---
+              # PCA
+              # ---
+              if (sum(i_pca) > 0) # only attributes left: vis [dim] [per]
               {
-                for (com in unique(subrequests_emb$COMPONENT))
+                # make the intermediate file
+                pca_loc <- get_inter_loc(requests[match(TRUE, i_pca),])
+                pca_result <- inter_readRDS(force, pca_loc)
+                if (is.null(pca_result))
                 {
-                  pca_loc <- sprintf("inter/%s_%s_%s.rds", table_name, emb, com)
-                  pca_result <- NULL
-                  if (force == 2 || !file.exists(pca_loc))
+                  sprintf_clean("Generating PCA INTER: %s", pca_loc)
+                  pca_result <- table_to_pca(col_table, com)
+                  mkdir_saveRDS(pca_result, pca_loc)
+                }
+
+                # make the final files
+                for (i in which(i_pca))
+                {
+                  r <- requests[i,]
+                  f_loc <- get_final_loc(r)
+                  sprintf_clean("Generating PCA FINAL: %s", f_loc)
+
+                  if (r$VISUALIZATION == "Explore")
+                    pca_to_explore(pca_result) %>% mkdir_saveRDS(f_loc)
+
+                  if (r$VISUALIZATION == "Summarize")
+                    pca_to_summary(pca_result) %>% mkdir_saveRDS(f_loc)
+
+                  if (r$VISUALIZATION == "tSNE")
+                    pca_to_tsne(pca_result, r$DIMENSION, r$PERPLEXITY) %>% mkdir_saveRDS(f_loc)
+
+                  times_done[i] <- Sys.time()
+                }
+              }
+
+              # ---
+              # VAE
+              # ---
+              for (bat in unique(requests$BATCH_SIZE[i_vae]))
+              {
+                i_bat <- i_vae & (requests$BATCH_SIZE == bat)
+
+                if (sum(i_bat) > 0) # only attributes left: vis [dim] [per]
+                {
+                  # make the intermediate file
+                  vae_loc <- get_inter_loc(requests[match(TRUE, i_bat),])
+                  vae_result <- inter_readRDS(force, vae_loc)
+                  if (is.null(vae_result))
                   {
-                    pca_result <- table_to_pca(nor_table, com)
-                    saveRDS(pca_result, pca_loc)
-                  }
-                  else
-                  {
-                    pca_result <- readRDS(pca_loc)
+                    sprintf_clean("Generating VAE INTER: %s", vae_loc)
+                    vae_result <- table_to_vae(col_table, com, bat)
+                    mkdir_saveRDS(vae_result, vae_loc)
                   }
 
-                  subrequests_com <- subrequests_emb[subrequests_emb$COMPONENT == com,]
-
-                  for (request in subrequests_com)
+                  # make the final files
+                  for (i in which(i_bat))
                   {
-                    if (request$VISUALIZATION == "Explore")
-                    {
-                      saveRDS(pca_to_explore(pca_result),
-                    }
+                    r <- requests[i,]
+                    f_loc <- get_final_loc(r)
+                    sprintf_clean("Generating VAE FINAL: %s", f_loc)
+
+                    if (r$VISUALIZATION == "Explore")
+                      vae_to_explore(vae_result) %>% mkdir_saveRDS(f_loc)
+
+                    if (r$VISUALIZATION == "Summarize")
+                      vae_to_summary(vae_result) %>% mkdir_saveRDS(f_loc)
+
+                    if (r$VISUALIZATION == "tSNE")
+                      vae_to_tsne(vae_result, r$DIMENSION, r$PERPLEXITY) %>% mkdir_saveRDS(f_loc)
+
+                    times_done[i] <- Sys.time()
                   }
                 }
               }
 
-              if (emb == "VAE")
+              # ----
+              # UMAP
+              # ----
+              for (per in unique(requests$PERPLEXITY[i_umap]))
               {
+                i_per <- i_umap & (requests$PERPLEXITY == per)
 
+                if (sum(i_per) > 0) # only attributes left: [dim]
+                {
+                  # make the intermediate file
+                  umap_loc <- get_inter_loc(requests[match(TRUE, i_per),])
+                  umap_result <- inter_readRDS(force, umap_loc)
+                  if (is.null(umap_result))
+                  {
+                    sprintf_clean("Generating INTER: %s", umap_loc)
+                    umap_result <- table_to_umap(col_table, com, per)
+                    mkdir_saveRDS(umap_result, umap_loc)
+                  }
+
+                  # make the final files
+                  for (i in which(i_per))
+                  {
+                    r <- requests[i,]
+                    f_loc <- get_final_loc(r)
+                    sprintf_clean("Generating UMAP FINAL: %s", f_loc)
+
+                    if (r$VISUALIZATION == "Explore")
+                      umap_to_explore(umap_result) %>% mkdir_saveRDS(f_loc)
+
+                    if (r$VISUALIZATION == "Summarize")
+                      umap_to_summary(umap_result) %>% mkdir_saveRDS(f_loc)
+
+                    if (r$VISUALIZATION == "tSNE")
+                      umap_to_tsne(umap_result, r$DIMENSION) %>% mkdir_saveRDS(f_loc)
+
+                    times_done[i] <- Sys.time()
+                  }
+                }
               }
 
-              if (emb == "UMAP")
+              # -----
+              # PHATE
+              # -----
+              for (i in which(i_phate)) # only attribute left: per
               {
+                r <- requests[i,]
+                f_loc <- get_final_loc(r)
+                sprintf_clean("Generating FINAL: %s", f_loc)
 
-              }
-
-              if (emb == "PHATE")
-              {
-
-              }
-
-              if (emb == "Sets")
-              {
-
+                table_to_phate(col_table, com, r$PERPLEXITY) %>% mkdir_saveRDS(f_loc)
+                times_done[i] <- Sys.time()
               }
             }
           }
@@ -414,10 +617,6 @@ perform_reduction <- function(requests, force = 0)
     }
   }
 
-  # table_to_pca(table, dim = 2), pca_to_summary(pca_result)
-  # table_to_vae(table, dim = 2, batch_size = 2), vae_to_summary(vae_result)
-  # table_to_umap(table, dim = 2, perp = 2), umap_to_summary(umap_result)
-  # table_to_phate(data, dim = 2, perp = 1), table_to_tsne(table, dim = 2, perp = 1)
-
-  # table_to_sets(data, thre), set_label_matrix(sets_result, labels)
+  requests$TIME_COMPLETED <- times_done
+  requests
 }
