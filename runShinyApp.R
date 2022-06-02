d88b9a49a20f7b6847300e4e81b28d5566407777
justin <justinchang1124@gmail.com>
2022-06-02 10:39:09 -0400

    style improvements

--- runShinyApp.R
+++ runShinyApp.R
@@ -1,3 +1,3 @@
-message('library paths:\n', paste('... ', .libPaths(), sep='', collapse='\n'))
-options(browser=file.path(getwd(),'GoogleChromePortable/App/Chrome-bin/chrome.exe'))
-shiny::runApp('app', launch.browser=T)
+message('library paths:\n', paste('... ', .libPaths(), sep='', collapse = '\n'))
+options(browser = file.path(getwd(), 'GoogleChromePortable/App/Chrome-bin/chrome.exe'))
+shiny::runApp('app', launch.browser = TRUE)
