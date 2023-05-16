4f7429ff0d6922b359f16f5d5d2defa9a214c7f3
justin <justinchang1124@gmail.com>
2023-05-16 17:50:22 -0400

    improved coding conventions

--- conventions.R
+++ conventions.R
@@ -1,18 +1,19 @@
 # The purpose of this file is to list coding conventions used in this project.
-# Each file begins with comments of this form, which summarize the purpose of the file.
+# Each file begins with comments like these, which summarize the file's purpose.
 
 # order of imports:
-# --check install
+# --check install.R
 # --import third-party libraries
-#     note: since we check install, use library not require
+#     install.R handles all edge cases
+#     so use library(), not require()
 # --source local code files
 
-# --------------
-# SECTION HEADER
-# --------------
+# -------------------------
+# SECTION HEADER (ALL CAPS)
+# -------------------------
 
 # settings: end in newline, no trailing whitespace, spaces only
-# aside from the opening comment, most comments are terse and minimally formatted
+# most code-describing comments are terse and minimally formatted
 # --comments use double hyphens to denote bullet points
 # --wrapping is done by using tabs (2 spaces)
 # when quoting an expression, double quotes are used on the outermost quote
@@ -23,10 +24,16 @@
 # bucket_case is used for function names
 # brackets are done according to K&R
 # redundant return statements are avoided
-# if wrapping an expression is necessary, end lines on an operator
+# if wrapping an expression is necessary, end lines on an operator (like +)
 # using "T", "F" as abbreviations for "TRUE", "FALSE" is strictly avoided
+# functions with comments should obey roxygen2 conventions
+# the function below demonstrates the above principles
 
-# this function demonstrates the above principles
+#' Manipulate two objects
+#'
+#' @param x An object.
+#' @param y An object.
+#' @returns A list.
 example_func <- function(a = 1, b = 2)
 {
   list("A test" = a, "B test" = b, "C test" = "test", "D test" = TRUE && TRUE && TRUE &&
