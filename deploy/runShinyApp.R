b0cbad2ed65ef3cf6dad665f77156a0aabe44851
justin <justinchang1124@gmail.com>
2023-05-17 11:09:53 -0400

    remove support for generating portable applications, revise the README

--- deploy/runShinyApp.R
+++
@@ -1,3 +0,0 @@
-message('library paths:\n', paste('... ', .libPaths(), sep='', collapse = '\n'))
-options(browser = file.path(getwd(), 'GoogleChromePortable/App/Chrome-bin/chrome.exe'))
-shiny::runApp('app', launch.browser = TRUE)
