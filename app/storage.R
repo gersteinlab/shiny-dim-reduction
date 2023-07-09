a7295883db79932042ed6fedb63696797e494de7
justin <justinchang1124@gmail.com>
2023-07-09 15:40:50 -0400

    storage refactoring

--- app/storage.R
+++ app/storage.R
@@ -15,20 +15,39 @@ library(aws.s3)
 # LOCAL STORAGE
 # -------------
 
+# is x in the correct format for a local store?
+# (note: can be in correct format and not connect)
 is_local_store <- function(x)
 {
   is_str(x)
 }
 
-local_is_connected <- function()
+# makes a local store object
+make_local_store <- function(dirname = "")
 {
-  dir.exists(Sys.getenv("LOCAL_STORE"))
+  stopifnot(is_local_store(dirname))
+  dirname
 }
 
-local_connects <- function(local_store)
+# set local store (not checked)
+set_local_store <- function(local_store)
 {
   Sys.setenv("LOCAL_STORE" = local_store)
-  local_is_connected()
+}
+
+# check if the local store is connected
+local_connected <- function()
+{
+  sdr_config$mode != "cloud" && dir.exists(Sys.getenv("LOCAL_STORE"))
+}
+
+# check local connection
+local_connects <- function(local_store)
+{
+  if (!is_local_store(local_store))
+    return(FALSE)
+  set_local_store(local_store)
+  local_connected()
 }
 
 # gives the local storage path for a location
@@ -63,10 +82,12 @@ load_local <- function(filename)
   readRDS(path_local(filename))
 }
 
-# --------------
-# KEY MANAGEMENT
-# --------------
+# -------------
+# CLOUD STORAGE
+# -------------
 
+# is x in the correct format for a cloud store?
+# (note: can be in correct format and not connect)
 is_cloud_store <- function(x)
 {
   members <- c("id", "secret", "bucket")
@@ -74,7 +95,7 @@ is_cloud_store <- function(x)
     is_str(x$id) && is_str(x$secret) && is_str(x$bucket)
 }
 
-# makes a list that qualifies as a key from an ID, a secret, and a bucket
+# makes a cloud store object
 make_cloud_store <- function(id = "", secret = "", bucket = "")
 {
   result <- list(
@@ -88,29 +109,34 @@ make_cloud_store <- function(id = "", secret = "", bucket = "")
   result
 }
 
-# checks if there exists a connection to AWS
-cloud_is_connected <- function()
-{
-  bucket_exists(Sys.getenv("AWS_ACCESS_BUCKET"))
-}
-
-# determines if cloud_store actually works
-cloud_connects <- function(cloud_store)
+# set cloud store (not checked)
+set_cloud_store <- function(cloud_store)
 {
   Sys.setenv(
     "AWS_ACCESS_KEY_ID" = cloud_store$id,
     "AWS_SECRET_ACCESS_KEY" = cloud_store$secret,
     "AWS_ACCESS_BUCKET" = cloud_store$bucket
   )
-
-  cloud_is_connected()
 }
 
-# --------------
-# AWS.S3 STORAGE
-# --------------
+# checks if there exists a connection to AWS
+cloud_connected <- function()
+{
+  tryCatch(
+    return(bucket_exists(Sys.getenv("AWS_ACCESS_BUCKET"))),
+    warning = function(e) return(FALSE),
+    error = function(e) return(FALSE)
+  )
+}
 
-my_amazon_obj <- NULL
+# check cloud connection
+cloud_connects <- function(cloud_store)
+{
+  if (!is_cloud_store(cloud_store))
+    return(FALSE)
+  set_cloud_store(cloud_store)
+  cloud_connected()
+}
 
 # lists the contents of a bucket's prefix
 list_aws_s3 <- function(prefix = NULL)
@@ -124,8 +150,15 @@ list_aws_s3 <- function(prefix = NULL)
 # determines whether a single object with the given filename exists
 find_aws_s3 <- function(filename)
 {
-  suppressMessages(object_exists(filename, Sys.getenv("AWS_ACCESS_BUCKET")))
+  tryCatch(
+    return(object_exists(filename, Sys.getenv("AWS_ACCESS_BUCKET"))),
+    warning = function(e) return(FALSE),
+    error = function(e) return(FALSE)
+  )
 }
+
+# for temporary use
+my_amazon_obj <- NULL
 
 # saves a single object to AWS.s3 - modified from s3save
 save_aws_s3 <- function(data, filename)
@@ -153,61 +186,32 @@ load_aws_s3 <- function(filename)
   my_amazon_obj
 }
 
-# ------------------
-# STORAGE ASSIGNMENT
-# ------------------
+# -------------
+# STORAGE QUERY
+# -------------
 
-set_storage_local <- function(ref)
+get_user_store_mode <- function()
 {
-  assign_global("list_store", list_local)
-  assign_global("find_store", find_local)
-  assign_global("save_store", save_local)
-  assign_global("load_store", load_local)
+  if (readline("Type 'Y' and press enter to use local storage.
+Type anything else and press enter to use AWS storage.") == "Y")
+    return("local")
+  "cloud"
 }
 
-set_storage_aws <- function(key)
+decide_store_mode <- function()
 {
-  assign_global("list_store", list_aws_s3)
-  assign_global("find_store", find_aws_s3)
-  assign_global("save_store", save_aws_s3)
-  assign_global("load_store", load_aws_s3)
-}
-
-query_storage <- function(ref = NULL, key = NULL)
-{
-  if (sdr_config$mode != "cloud" && is_valid_ref(ref)) # can use local storage
+  if (local_connects(local_store))
   {
-    if (is_valid_key(key)) # can use AWS storage
-    {
-      if ("Y" == readline(prompt = "
-Type 'Y' and press enter to use local storage.
-Type anything else and press enter to use AWS storage. "))
-      {
-        set_storage_local(ref)
-        return(TRUE)
-      }
-      else
-      {
-        set_storage_aws(key)
-        return(FALSE)
-      }
-    }
-    else # cannot use AWS storage
-    {
-      set_storage_local(ref)
-      return(TRUE)
-    }
+    if (cloud_connects(cloud_store))
+      return(get_user_store_mode())
+    else
+      return("local")
   }
-  else # cannot use local storage
+  else
   {
-    if (is_valid_key(key)) # can use AWS storage
-    {
-      set_storage_aws(key)
-      return(FALSE)
-    }
-    else # cannot use AWS storage
-    {
-      stop("Neither local storage nor AWS storage are available.")
-    }
+    if (cloud_connects(cloud_store))
+      return("cloud")
+    else
+      stop("Local / cloud connections failed.")
   }
 }
