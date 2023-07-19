19893f251b33fdacb6726e230ec8ca625db7ab68
justin <justinchang1124@gmail.com>
2023-07-19 17:31:43 -0400

    document authentication

--- app/authentication.R
+++ app/authentication.R
@@ -1,4 +1,6 @@
-# The purpose of this file is to provide functions for bcrypt authentication.
+# The purpose of this file is to provide functions for
+# bcrypt authentication. It is a minimum viable example
+# and should not be used for protecting user data.
 
 if (!exists("sdr_config"))
   source("app/install.R")
@@ -6,16 +8,20 @@ if (!exists("sdr_config"))
 library(bcrypt)
 
 #' whether x is a 'credentials' object
+#' note: minimum password length of 6 from NIST 2023.
 #'
 #' @param x An object.
 #' @returns TRUE or FALSE.
 are_credentials <- function(x)
 {
   is.character(x) &&
-    is.character(names(x)) && is_unique(names(x)) && none_na(names(x))
+    are_list_names(names(x)) &&
+    all(nchar(x) >= 6)
 }
 
-# for changing what the credentials are
+#' sets the global credentials object to x
+#'
+#' @param x A credentials object.
 assign_global("credentials", list())
 set_credentials <- function(x)
 {
@@ -23,17 +29,24 @@ set_credentials <- function(x)
   assign_global("credentials", x)
 }
 
-# straightforward password hashing
+#' hashes the provided password
+#'
+#' @param x A string.
+#' @returns A string.
 my_hash <- function(password)
 {
   stopifnot(is_str(password))
   bcrypt::hashpw(password, gensalt(12))
 }
 
-# checks if credentials[[username]] == password
+#' checks if a username / password authenticates
+#'
+#' @param x A string.
+#' @returns A string.
 my_auth <- function(username, password)
 {
   stopifnot(exists("credentials"))
+  # aka: credentials[[username]] == hashpw(password)
   is_str(username) && is_str(password) &&
     (username %in% names(credentials)) &&
     bcrypt::checkpw(password, credentials[[username]])
